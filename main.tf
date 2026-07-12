26afa92837531713d4fd591ecfefaabe447e7e4b
danders929 <sandersda@gmail.com>
2026-07-11 22:57:21 -0500

    fix: update azure public IP allocation to static standard SKU

--- main.tf
+++ main.tf
@@ -37,7 +37,8 @@ resource "azurerm_public_ip" "public_ip" {
   name                = "devops-public-ip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
+  sku = "Standard"
 }
 
 # 4. Create Network Security Group (Firewall)
